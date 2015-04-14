e85740293d3d45ae3796eaf14da2850634f73250
Eduardo Cuducos <cuducos@gmail.com>
2015-04-14 20:42:57 +0100

    Use GitHub API

--- getgist.py
+++ getgist.py
@@ -1,93 +1,78 @@
 # coding: utf-8
 
 import argparse
+import json
 import os
-import re
 import urllib2
 
 
-class file_name(object):
+class Gist(object):
 
-    def __init__(self, user=None, file_name=None):
+    def __init__(self, user=False, file_name=False, assume_yes=False):
+
+        # set main variables
+        self.user = user
+        self.file_name = file_name
+        self.assume_yes = assume_yes
 
         # load arguments
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-y', '--yes-to-all',
-                            help='Assume `yes` to all prompts',
-                            action="store_true")
-        parser.add_argument('user', help='Gist username')
-        parser.add_argument('file_name', help='Gist file name')
-        args = parser.parse_args()
+        if not self.user or not self.file_name:
 
-        # set http request variables
-        self.user = args.user
-        self.file_name = args.file_name
-        self.domain = 'https://gist.github.com'
-        self.assume_yes = args.yes_to_all
+            # set argparse
+            parser = argparse.ArgumentParser()
+            parser.add_argument('user', help='Gist username')
+            parser.add_argument('file_name', help='Gist file name')
+            parser.add_argument('-y', '--yes-to-all',
+                                help='Assume `yes` to all prompts',
+                                action="store_true")
 
-        # current directory
-        self.current = os.path.dirname(os.path.realpath(__file__))
+            # load values from argparse
+            args = parser.parse_args()
+            if not user:
+                self.user = args.user
+            if not file_name:
+                self.file_name = args.file_name
+            if not assume_yes:
+                self.assume_yes = args.yes_to_all
 
-        # regex to find a link tag – use .format(…) to include tag contents
-        self.link_tag = r'<a[\w\s=:.,-?i\'"</>]+{}[</>\w]*<\/a>'
+        # set support variables
+        self.local_dir = os.path.dirname(os.path.realpath(__file__))
+        self.local_path = os.path.join(self.local_dir, self.file_name)
+        self.info = self.__load_gist()
 
-        # regexp to find a href attribute
-        self.href_attribute = r'href=[\'"][\w/\/.:=?]+[\'"]'
+    @property
+    def id(self):
+        return self.info.get('id', None)
 
-    def run(self):
-        gist_url = self.__get_url(self.user, self.file_name)
-        if gist_url:
-            gist_raw_url = self.__get_url(gist_url, 'Raw')
-            if gist_raw_url:
-                contents = self.__get_contents(gist_raw_url)
-                self.__save(contents)
-            else:
-                print "  Couldn't fetch gist raw URL"
-        else:
-            print "  Couldn't fetch gist URL"
+    @property
+    def raw_url(self):
+        return self.info.get('raw_url', None)
 
-    def __get_url(self, location, name):
-        data = self.__get_contents(location)
-        if data:
-            tag = self.__match(self.link_tag.format(name), data)
-            if tag:
-                gist_url = self.__match(self.href_attribute, tag)
-                if gist_url:
-                    return gist_url[6:-1]
+    def __load_gist(self):
+        url = 'https://api.github.com/users/{}/gists'.format(self.user)
+        gists = json.loads(self.__curl(url))
+        for gist in gists:
+            if self.file_name in gist['files']:
+                return {'id': gist['id'],
+                        'raw_url': gist['files'][self.file_name]['raw_url']}
         return False
 
-    def __match(self, regex, data):
-        data = data.replace('href="/{0}">{0}</a>'.format(self.user), '')
-        search = re.search(regex, data)
-        if search:
-            return search.group(0)
-        else:
-            print "  Couldn't match regular expression"
-            print "  regexp: ", regex
-            print "  data: ", data[0:140], '…'
-            return search
-
-    def __get_contents(self, location):
-        if location[0:1] != '/':
-            location = '/{}'.format(location)
-        url = '{}{}'.format(self.domain, location)
-        print "  Fetching {} ...".format(url)
+    def __curl(self, url):
         request = urllib2.urlopen(url)
+        self.__output('Fetching {} …'.format(url))
         status = request.getcode()
         if status == 200:
             return request.read()
-        else:
-            print status
-            print request.info()
-            return False
+        self.__output('[Fail] HTTP Status {}'.format(url, status))
+        return False
 
-    def __save(self, contents):
+    def __output(self, message):
+        print('  {}'.format(message))
 
-        # currentd ir and file path
-        file_path = os.path.join(self.current, self.file_name)
+    def save(self):
 
         # check if file exists
-        if os.path.exists(file_path):
+        if os.path.exists(self.local_path):
 
             # delete or backup existing file?
             confirm = 'y'
@@ -97,29 +82,31 @@ class file_name(object):
 
             # delete exitsing file
             if confirm.lower() == 'y':
-                print "  Deleting existing {} …".format(self.file_name)
-                os.remove(file_path)
+                self.__output('Deleting existing {} …'.format(self.file_name))
+                os.remove(self.local_path)
 
             # backup existing file
             else:
                 self.__backup()
 
         # save new file
-        with open(file_path, 'w') as file_handler:
-            print "  Saving new {} …".format(self.file_name)
+        with open(self.local_path, 'w') as file_handler:
+            contents = self.__curl(self.raw_url)
+            self.__output('Saving new {} …'.format(self.file_name))
             file_handler.write(contents)
-        print "  Done!"
+        self.__output('Done!')
 
     def __backup(self):
         count = 0
         name = '{}.bkp'.format(self.file_name)
-        backup = os.path.join(self.current, name)
+        backup = os.path.join(self.local_dir, name)
         while os.path.exists(backup):
             count += 1
             name = '{}.bkp.{}'.format(self.file_name, count)
-            backup = os.path.join(self.current, name)
-        print "  Saving existing {} as {}".format(self.file_name, name)
-        os.rename(os.path.join(self.current, self.file_name), backup)
+            backup = os.path.join(self.local_dir, name)
+        self.__output('Moving existing {} to {}…'.format(self.file_name, name))
+        os.rename(os.path.join(self.local_dir, self.file_name), backup)
 
-run_forrest = file_name()
-run_forrest.run()
+if __name__ == '__main__':
+    gist = Gist()
+    gist.save()
---
+++ tests/__init__.py
@@ -0,0 +1,40 @@
+import os
+from getgist.getgist import Gist
+from re import search
+from unittest import TestCase
+from urllib2 import urlopen
+
+test_user = 'cuducos'
+test_gist = '.vimrc'
+
+
+class TestAPI(TestCase):
+
+    def setUp(self):
+        self.gist = Gist(test_user, test_gist)
+
+    def test_id(self):
+        self.assertTrue(search(r'[\w\d]{16,}', self.gist.id))
+
+    def test_raw_url(self):
+        request = urlopen(self.gist.raw_url)
+        self.assertEqual(request.getcode(), 200)
+
+
+class TestDownload(TestCase):
+
+    def setUp(self):
+        self.gist = Gist(test_user, test_gist)
+
+    def tearDown(self):
+        for file_name in os.listdir(self.gist.local_dir):
+            if search(r'^({})(\.bkp(.\d)?)?$'.format(test_gist), file_name):
+                os.remove(os.path.join(self.gist.local_dir, file_name))
+
+    def test_local_dir(self):
+        self.assertTrue(os.path.exists(self.gist.local_dir))
+        self.assertTrue(os.path.isdir(self.gist.local_dir))
+
+    def test_download(self):
+        self.gist.save()
+        self.assertTrue(os.path.exists(self.gist.local_path))
