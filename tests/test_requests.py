ad30214a7cbfae3597c54d959a70142c90404d99
Eduardo Cuducos <cuducos@gmail.com>
2016-01-17 17:40:59 -0200

    Add tests for GetGistRequests

---
+++ tests/test_requests.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from getgist.local import LocalTools
+from getgist.requests import GetGistRequests
+
+
+class TestGetGistRequestsInit(TestCase):
+
+    def test_no_header(self):
+        requests = GetGistRequests()
+        self.assertIsInstance(requests.headers, dict)
+
+    def test_update_header(self):
+        requests = GetGistRequests({'foo': 'bar'})
+        self.assertEqual(requests.headers['foo'], 'bar')
+
+
+class TestGetGistRequestsAddHeader(TestCase):
+
+    def test_add_header(self):
+        requests = GetGistRequests({'foo': 'bar'})
+        got = requests.add_headers({'headers': {'bar': 'foo'}})
+        self.assertEqual({'headers': {'foo': 'bar', 'bar': 'foo'}}, got)
