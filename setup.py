c4b21d8f7fae309e5256f38ad4f124f05090b71e
Eduardo Cuducos <cuducos@users.noreply.github.com>
2019-04-17 14:33:27 -0300

    Add Python 3.7 to tox and setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ with open("README.rst") as readme:
             "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
             "Intended Audience :: Developers",
             "Topic :: Utilities",
             "License :: OSI Approved :: MIT License",
