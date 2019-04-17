8b79c0ea60e98e2b4e557eedf62047fa6e3159bd
Eduardo Cuducos <cuducos@users.noreply.github.com>
2019-04-17 14:24:38 -0300

    Simplify add headers API in getgist.request

--- getgist/request.py
+++ getgist/request.py
@@ -14,24 +14,23 @@ class GetGistRequests(object):
             headers = dict()
         self.headers = headers
 
-    def add_headers(self, kwargs):
+    def add_headers(self, **headers):
         """
         Add any extra header to the existng header object.
-        :param kwargs: (dict)
-        :return: (dict) containing an item with `headers` as key
+        :param kwargs: key/value pairs
+        :return: (dict)
         """
-        kwargs["headers"] = kwargs.get("headers", {})
-        kwargs["headers"].update(self.headers)
-        return kwargs
+        headers.update(self.headers)
+        return headers
 
     def get(self, url, params=None, **kwargs):
         """Encapsulte requests.get to use this class instance header"""
-        return requests.get(url, params=params, **self.add_headers(kwargs))
+        return requests.get(url, params=params, headers=self.add_headers(**kwargs))
 
     def patch(self, url, data=None, **kwargs):
         """Encapsulte requests.patch to use this class instance header"""
-        return requests.patch(url, data=data, **self.add_headers(kwargs))
+        return requests.patch(url, data=data, headers=self.add_headers(**kwargs))
 
     def post(self, url, data=None, **kwargs):
         """Encapsulte requests.post to use this class instance header"""
-        return requests.post(url, data=data, **self.add_headers(kwargs))
+        return requests.post(url, data=data, headers=self.add_headers(**kwargs))
--- tests/test_requests.py
+++ tests/test_requests.py
@@ -13,6 +13,59 @@ def test_update_header():
 
 def test_add_header():
     requests = GetGistRequests({"foo": "bar"})
-    new_headers = requests.add_headers({"headers": {"bar": "foo"}})
-    assert new_headers == {"headers": {"foo": "bar", "bar": "foo"}}
-    # TODO new API for add_headers to avoid the repetitive usage of `header`
+    new_headers = requests.add_headers(bar="foo")
+    assert new_headers == {"foo": "bar", "bar": "foo"}
+
+
+def test_get_without_params(mocker):
+    get = mocker.patch("getgist.request.requests.get")
+    requests = GetGistRequests({"foo": "bar"})
+    requests.get("foobar", bar="foo")
+    get.assert_called_once_with(
+        "foobar", params=None, headers={"foo": "bar", "bar": "foo"}
+    )
+
+
+def test_get_with_params(mocker):
+    get = mocker.patch("getgist.request.requests.get")
+    requests = GetGistRequests({"foo": "bar"})
+    requests.get("foobar", params=42, bar="foo")
+    get.assert_called_once_with(
+        "foobar", params=42, headers={"foo": "bar", "bar": "foo"}
+    )
+
+
+def test_post_without_data(mocker):
+    post = mocker.patch("getgist.request.requests.post")
+    requests = GetGistRequests({"foo": "bar"})
+    requests.post("foobar", bar="foo")
+    post.assert_called_once_with(
+        "foobar", data=None, headers={"foo": "bar", "bar": "foo"}
+    )
+
+
+def test_post_with_data(mocker):
+    post = mocker.patch("getgist.request.requests.post")
+    requests = GetGistRequests({"foo": "bar"})
+    requests.post("foobar", data=42, bar="foo")
+    post.assert_called_once_with(
+        "foobar", data=42, headers={"foo": "bar", "bar": "foo"}
+    )
+
+
+def test_patch_without_data(mocker):
+    patch = mocker.patch("getgist.request.requests.patch")
+    requests = GetGistRequests({"foo": "bar"})
+    requests.patch("foobar", bar="foo")
+    patch.assert_called_once_with(
+        "foobar", data=None, headers={"foo": "bar", "bar": "foo"}
+    )
+
+
+def test_patch_with_data(mocker):
+    patch = mocker.patch("getgist.request.requests.patch")
+    requests = GetGistRequests({"foo": "bar"})
+    requests.patch("foobar", data=42, bar="foo")
+    patch.assert_called_once_with(
+        "foobar", data=42, headers={"foo": "bar", "bar": "foo"}
+    )
